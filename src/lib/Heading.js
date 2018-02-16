eca95d3decab0383d23fc9a3766e15f56d101b7e
Peter Morawiec <petermorawiec@gmail.com>
2018-02-15 19:32:10 -0800

    rework Heading, and add support for h5

--- src/lib/Heading.js
+++ src/lib/Heading.js
@@ -23,6 +23,7 @@ const propTypes = {
   h2: PropTypes.bool,
   h3: PropTypes.bool,
   h4: PropTypes.bool,
+  h5: PropTypes.bool,
   color: PropTypes.string,
   lineHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   margin: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
@@ -43,13 +44,41 @@ const getCss = props => {
   `;
 };
 
-// NOTE: Not using withComponent(), because reasons..
+const Heading1 = styled.h1`
+  ${props => getCss(addTheme(props))};
+`;
+
+const Heading2 = styled.h2`
+  ${props => getCss(addTheme(props))};
+`;
+
+const Heading3 = styled.h3`
+  ${props => getCss(addTheme(props))};
+`;
+
+const Heading4 = styled.h4`
+  ${props => getCss(addTheme(props))};
+`;
+
+const Heading5 = styled.h5`
+  ${props => getCss(addTheme(props))};
+`;
+
 const Heading = props => {
-  const hTag = ['h1', 'h2', 'h3', 'h4'].find(hTag => hTag in props) || 'h1';
-  const H = styled[hTag]`
-    ${getCss(addTheme({ ...props }))};
-  `; // had to clone, otherwise "object not extensible" error
-  return <H>{props.children}</H>;
+  const hTag = ['h1', 'h2', 'h3', 'h4', 'h5'].find(hTag => hTag in props);
+  switch (hTag) {
+    case 'h5':
+      return <Heading5 {...props}>{props.children}</Heading5>;
+    case 'h4':
+      return <Heading4 {...props}>{props.children}</Heading4>;
+    case 'h3':
+      return <Heading3 {...props}>{props.children}</Heading3>;
+    case 'h2':
+      return <Heading2 {...props}>{props.children}</Heading2>;
+    default:
+      return <Heading1 {...props}>{props.children}</Heading1>;
+  }
 };
+
 Heading.propTypes = propTypes;
 export default Heading;
